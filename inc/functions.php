5db41bc8d5a99808871b8e4526b3c1b145d07e6d
Rebecca Bott <faithbott@gmail.com>
2019-09-06 12:58:50 -0400

    added the starter files

---
+++ inc/functions.php
@@ -0,0 +1,13 @@
+// PHP - Random Quote Generator
+<?php
+
+// Create the Multidimensional array of quote elements and name it quotes
+// Each inner array element should be an associative array
+
+
+
+// Create the getRandomQuuote function and name it getRandomQuote
+
+
+
+// Create the printQuote funtion and name it printQuote
